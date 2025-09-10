29c1da19480b9e04001c832709fe17ab32da842d
Lara Ferraz <larapfz06@gmail.com>
2025-09-09 21:40:51 -0300

    alterações

--- php/db_config.php
+++ php/db_config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "SkillMatch";
+$dbname = "skillmatch";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
